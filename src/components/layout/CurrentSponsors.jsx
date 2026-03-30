f620dd9ca852225d8c3aeb41379e274a4c2cb711
Zack Hoang <zackhoang2004@gmail.com>
2026-03-30 08:23:19 -0700

    Remove <figure> and <figcaption> and reduce image size

--- src/components/layout/CurrentSponsors.jsx
+++ src/components/layout/CurrentSponsors.jsx
@@ -5,12 +5,9 @@ export function CurrentSponsors() {
     return (
         <section className="text-center">
             <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[48px] font-heroDate font-extrabold leading-none text-text-primary mb-4">Sponsors</h2>
-            <div className="flex flex-col gap-20 md:grid md:grid-cols-3 md:items-center">
+            <div className="flex flex-col gap-20 items-center md:grid md:grid-cols-3 md:items-center">
                 {currentSponsors.map((sponsor, index) => {
-                    return <figure className="flex flex-col items-center" key={index}>
-                        <img className="w-3/4" src={sponsor.logo} alt={sponsor.alt}></img>
-                        <figcaption className="text-gray-600 text-xl">{sponsor.name}</figcaption>
-                    </figure>
+                    return<img key={index} className="w-1/2 flex justify-self-center" src={sponsor.logo} alt={sponsor.alt}></img>
                 })}
             </div>
         </section>
